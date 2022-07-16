e35a9cc865f1b15dbb8701a540e241fc3e1c9f3d
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-07-16 20:09:28 +0200

    Update botest.py

--- botest.py
+++ botest.py
@@ -4,7 +4,6 @@ API_TOKEN = '5567408807:AAGamuBetRhvu2OHik91UmV5sK8AuOzrK9k'
 
 bot = telebot.TeleBot(API_TOKEN)
 
-
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
@@ -12,7 +11,10 @@ def send_welcome(message):
 Ciao, per favore mandami la tua posizione per trovarti il toret piu' vicino\
 """)
 
-
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message,"""Non posso capirlo, per favore mandami la tua posizione per trovarti il toret piu' vicino\
+""")
 
 bot.polling()
