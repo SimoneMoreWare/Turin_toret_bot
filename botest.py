f00d14505b429e169abcd33e1c423a8e6de599a7
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-07-17 16:02:15 +0200

    Update botest.py
    ho aggiunto lettura file

--- botest.py
+++ botest.py
@@ -1,8 +1,32 @@
 import telebot
+FILENAME = 'touretmap.txt'
 
 API_TOKEN = '5567408807:AAGamuBetRhvu2OHik91UmV5sK8AuOzrK9k'
 
 bot = telebot.TeleBot(API_TOKEN)
+
+def leggidati(file_name):
+    dati = []
+    try:
+        turet_file = open(file_name, 'r')
+    except:
+        print("errore file ")
+    next(turet_file) #salto la prima riga
+    for line in turet_file:
+        line=line.strip().split(",")
+        value=[]
+        nome=str(line[0])
+        value.append(nome)
+        id=int(line[1])
+        value.append(id)
+        lan=float(line[2])
+        value.append(lan)
+        lng=float(line[3])
+        value.append(lng)
+        dati.append(value)
+
+    turet_file.close()
+    return dati
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
@@ -24,8 +48,12 @@ def location_received(message):
     print(message)
     bot.send_message(message.chat.id,"Località ricevuta")
     bot.send_message(message.chat.id,"Ricerca per il toret piu' vicino...")
-    bot.send_location(message.chat.id,  message.location.latitude , message.location.longitude)
+    searchnearturet(message.location.latitude, message.location.longitude,message)
+
+@bot.message_handler(content_types=["location"])
+def searchnearturet(lat_current,lng_current,message):
+    dati=leggidati(FILENAME)
+    bot.send_location(message.chat.id,  lat_current , lng_current)
     bot.send_message(message.chat.id,"Ecco il toret piu' vicino :)")
 
 bot.polling()
-
